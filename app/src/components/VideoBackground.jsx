e144b9aa1137fb00b1b81efdd096e9ab0975f2c5
Ahmad <53348451+ahmad420@users.noreply.github.com>
2023-03-10 10:24:26 +0200

    Update VideoBackground.jsx

--- app/src/components/VideoBackground.jsx
+++ app/src/components/VideoBackground.jsx
@@ -4,9 +4,9 @@ import "../App.css";
 const VideoBackground = () => {
   return (
     <video autoPlay loop muted>
-      <source src="../videos/ai.mp4" type="video/mp4" playsinline />
-      <source src="../videos/ai.mp4" type="video/webm" playsinline />
-      <source src="../videos/ai.mp4" type="video/ogg" playsinline />
+      <source src="../videos/ai.mp4" type="video/mp4" playsinline preload="auto" />
+      <source src="../videos/ai.mp4" type="video/webm" playsinline preload="auto"/>
+      <source src="../videos/ai.mp4" type="video/ogg" playsinline preload="auto" />
       {/* Fallback for browsers that don't support HTML5 video */}
       <img src="../images/ai.png" alt="video background" />
     </video>
